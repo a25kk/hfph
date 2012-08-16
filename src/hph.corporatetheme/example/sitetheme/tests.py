e2d11a30c104d901aa092e03ec137ccb85d7e50b
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-08-16 13:00:34 +0200

    Include basic theme temüplate "corporate"

--- src/hph.corporatetheme/example/sitetheme/tests.py
+++ src/hph.corporatetheme/example/sitetheme/tests.py
@@ -9,7 +9,7 @@ from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
 ptc.setupPloneSite()
 
-import example.sitetheme
+import hph.corporatetheme
 
 
 class TestCase(ptc.PloneTestCase):
@@ -19,7 +19,7 @@ class TestCase(ptc.PloneTestCase):
         @classmethod
         def setUp(cls):
             fiveconfigure.debug_mode = True
-            ztc.installPackage(example.sitetheme)
+            ztc.installPackage(hph.corporatetheme)
             fiveconfigure.debug_mode = False
 
         @classmethod
@@ -32,21 +32,21 @@ def test_suite():
 
         # Unit tests
         #doctestunit.DocFileSuite(
-        #    'README.txt', package='example.sitetheme',
+        #    'README.txt', package='hph.corporatetheme',
         #    setUp=testing.setUp, tearDown=testing.tearDown),
 
         #doctestunit.DocTestSuite(
-        #    module='example.sitetheme.mymodule',
+        #    module='hph.corporatetheme.mymodule',
         #    setUp=testing.setUp, tearDown=testing.tearDown),
 
 
         # Integration tests that use PloneTestCase
         #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='example.sitetheme',
+        #    'README.txt', package='hph.corporatetheme',
         #    test_class=TestCase),
 
         #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='example.sitetheme',
+        #    'browser.txt', package='hph.corporatetheme',
         #    test_class=TestCase),
 
         ])
--- src/hph.corporatetheme/setup.py
+++ src/hph.corporatetheme/setup.py
@@ -3,7 +3,7 @@ import os
 
 version = '1.0'
 
-setup(name='example.sitetheme',
+setup(name='hph.corporatetheme',
       version=version,
       description="Example Diazo Plone Theme",
       long_description=open("README.txt").read() + "\n" +
