320310b858ca12cf6d164f164abf2552e575e44b
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-09-26 15:43:09 +0200

    Re-factor content types to make them usable

--- src/hph.sitecontent/hph/sitecontent/newsentry.py
+++ src/hph.sitecontent/hph/sitecontent/newsentry.py
@@ -1,32 +1,28 @@
 from five import grok
 from plone.directives import dexterity, form
 
-from zope import schema
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-from zope.interface import invariant, Invalid
-
-from z3c.form import group, field
-
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.field import NamedBlobImage
 
 from plone.app.textfield import RichText
 
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
-
 from hph.sitecontent import MessageFactory as _
 
-
-# Interface class; used to define content-type schema.
 
 class INewsEntry(form.Schema, IImageScaleTraversable):
     """
     A news or announcement item
     """
+    image = NamedBlobImage(
+        title=_(u"Preview Image"),
+        description=_(u"Upload a preview image that will be used in news "
+                      u"teasers."),
+        required=True,
+    )
+    text = RichText(
+        title=_(u"Main Text"),
+        required=True,
+    )
 
 
 class NewsEntry(dexterity.Container):
