5b021dd0bbf7b8167d99fc010c3a546d46679f38
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-09-26 15:24:46 +0200

    Re-factor the ticker bar

--- src/hph.bulletinboard/hph/bulletinboard/tickerbar.py
+++ src/hph.bulletinboard/hph/bulletinboard/tickerbar.py
@@ -1,9 +1,14 @@
+import DateTime
 from five import grok
+from Acquisition import aq_inner
 from zope.interface import Interface
 from zope.component import getMultiAdapter
+
+from Products.CMFCore.utils import getToolByName
+
 from plone.app.layout.viewlets.interfaces import IPortalFooter
 
-from plone.app.layout.navigation.interfaces import INavigationRoot
+from hph.bulletinboard.bulletin import IBulletin
 
 
 class TickerViewlet(grok.Viewlet):
@@ -14,5 +19,17 @@ class TickerViewlet(grok.Viewlet):
 
     def update(self):
         pstate = getMultiAdapter((self.context, self.request),
-                    name='plone_portal_state')
+                                 name='plone_portal_state')
         self.portal_url = pstate.portal_url
+        self.available = len(self.active_bulletins()) > 0
+
+    def active_bulletins(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        end = DateTime.DateTime() + 0.1
+        start = DateTime.DateTime() - 2
+        date_range_query = {'query': (start, end), 'range': 'min: max'}
+        results = catalog(object_provides=IBulletin.__identifier__,
+                          effective=date_range_query,
+                          review_state='published')
+        return results
