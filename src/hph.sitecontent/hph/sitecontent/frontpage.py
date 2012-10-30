aed98b272b9dbce885e1f8caf4d092ceb14dd95c
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-10-30 14:00:36 +0100

    Commit changes

--- src/hph.sitecontent/hph/sitecontent/frontpage.py
+++ src/hph.sitecontent/hph/sitecontent/frontpage.py
@@ -1,9 +1,35 @@
+from Acquisition import aq_inner
 from five import grok
+from Products.CMFCore.utils import getToolByName
 
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.contentlisting.interfaces import IContentListing
+
+from hph.sitecontent.eventitem import IEventItem
+from hph.sitecontent.newsentry import INewsEntry
 
 
 class FrontpageView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
     grok.name('frontpage-view')
+
+    def update(self):
+        self.has_events = len(self.eventitems()) > 0
+        self.has_news = len(self.recent_news()) > 0
+
+    def eventitems(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(object_provides=IEventItem.__identifier__,
+                          review_state='published')
+        resultlist = IContentListing(results)
+        return resultlist
+
+    def recent_news(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(object_provides=INewsEntry.__identifier__,
+                          review_state='published')
+        resultlist = IContentListing(results)
+        return resultlist
