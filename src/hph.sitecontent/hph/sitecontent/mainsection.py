e51081cc4c0705edcc5c645dff9c632206e9b91b
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-09-16 15:26:35 +0200

    Prepare dynamic section image inclusion and fix brancbar

--- src/hph.sitecontent/hph/sitecontent/mainsection.py
+++ src/hph.sitecontent/hph/sitecontent/mainsection.py
@@ -1,12 +1,14 @@
+from Acquisition import aq_inner
 from five import grok
 from plone.directives import dexterity, form
+from zope.component import getMultiAdapter
 
 from zope import schema
 
 from z3c.form import group, field
 
 from plone.namedfile.interfaces import IImageScaleTraversable
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.field import NamedBlobImage
 
 from plone.app.textfield import RichText
 
@@ -17,6 +19,10 @@ class IMainSection(form.Schema, IImageScaleTraversable):
     """
     Forder to represent the main site sections
     """
+    image = NamedBlobImage(
+        title=_(u"Banner image"),
+        required=False,
+    )
 
 
 class MainSection(dexterity.Container):
@@ -27,3 +33,14 @@ class View(grok.View):
     grok.context(IMainSection)
     grok.require('zope2.View')
     grok.name('view')
+
+    def constructImageTag(self):
+        obj = aq_inner(self.context)
+        scales = getMultiAdapter((obj, self.request), name='images')
+        scale = scales.scale('image', width=870, height=421)
+        data = {}
+        if scale is not None:
+            data['url'] = scale.url
+            data['width'] = scale.width
+            data['height'] = scale.height
+        return data
