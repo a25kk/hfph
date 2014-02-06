eb060bf7ec13d4d92fefede61712091b958716ab
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-06 13:15:55 +0100

    Add auto cleanup view

--- src/hph.sitecontent/hph/sitecontent/frontpage.py
+++ src/hph.sitecontent/hph/sitecontent/frontpage.py
@@ -106,3 +106,24 @@ class CleanupView(grok.View):
         results = catalog(portal_type=ptypes)
         items = IContentListing(results)
         return items
+
+
+class AutoCleanupFiles(grok.View):
+    grok.context(INavigationRoot)
+    grok.require('zope2.View')
+    grok.name('cleanup-files')
+
+    def render(self):
+        idx = 0
+        for item in self.to_cleanup():
+            api.content.delete(obj=item.getObject())
+            idx += 1
+        msg = 'File and images removed: {0}'.format(idx)
+        return msg
+
+    def to_cleanup(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        ptypes = ['Image', 'File']
+        results = catalog(portal_type=ptypes)
+        items = IContentListing(results)
+        return items
