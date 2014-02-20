77e8c4aa0fd97dd5fae37b629b037bdbf6b0cd25
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-20 13:43:46 +0100

    Move content filtering to dedicated view snippet

--- src/hph.faculty/hph/faculty/facultydirectory.py
+++ src/hph.faculty/hph/faculty/facultydirectory.py
@@ -2,6 +2,8 @@ from Acquisition import aq_inner
 from five import grok
 from plone import api
 
+from zope.schema.vocabulary import getVocabularyRegistry
+
 from plone.dexterity.content import Container
 from plone.directives import form
 from plone.namedfile.interfaces import IImageScaleTraversable
@@ -32,11 +34,16 @@ class View(grok.View):
     def filtered(self):
         return self.filter is True
 
+    def content_filter(self):
+        context = aq_inner(self.context)
+        tmpl = context.restrictedTraverse('@@content-filter-faculty')()
+        return tmpl
+
     def faculty_members(self):
         catalog = api.portal.get_tool(name='portal_catalog')
         query = self._base_query()
         if self.filter:
-            query['position'] = self.filter
+            query['position'] = self.request.get('academicRole', '')
         results = catalog(query)
         return results
 
@@ -48,3 +55,15 @@ class View(grok.View):
         return dict(object_provides=obj_provides,
                     path=query_path,
                     review_state='published')
+
+
+class ContentFilter(grok.View):
+    grok.context(IFacultyDirectory)
+    grok.require('zope2.View')
+    grok.name('content-filter-faculty')
+
+    def filter_options(self):
+        context = aq_inner(self.context)
+        vr = getVocabularyRegistry()
+        vocab = vr.get(context, 'hph.faculty.academicRole')
+        return vocab
