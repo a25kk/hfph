731f6f8a67d1af28003dec8d10fb12ca8516a5c6
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-20 12:36:49 +0100

    Refactor faculty member filtering

--- src/hph.faculty/hph/faculty/facultydirectory.py
+++ src/hph.faculty/hph/faculty/facultydirectory.py
@@ -1,9 +1,12 @@
+from Acquisition import aq_inner
 from five import grok
 from plone import api
 
 from plone.dexterity.content import Container
 from plone.directives import form
 from plone.namedfile.interfaces import IImageScaleTraversable
+
+from hph.faculty.facultymember import IFacultyMember
 
 from hph.faculty import MessageFactory as _
 
@@ -28,3 +31,20 @@ class View(grok.View):
 
     def filtered(self):
         return self.filter is True
+
+    def faculty_members(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        query = self._base_query()
+        if self.filter:
+            query['position'] = self.filter
+        results = catalog(query)
+        return results
+
+    def _base_query(self):
+        context = aq_inner(self.context)
+        obj_provides = IFacultyMember.__identifier__
+        query_path = dict(query='/'.join(context.getPhysicalPath()),
+                          depth=1),
+        return dict(object_provides=obj_provides,
+                    path=query_path,
+                    review_state='published')
