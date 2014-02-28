05fb6d5978367ee29216418140724c8b9597b895
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-28 13:43:39 +0100

    Add container for member workspaces

--- src/hph.membership/hph/membership/tests.py
+++ src/hph.membership/hph/membership/tests.py
@@ -52,6 +52,14 @@ def test_suite():
 
         # -*- extra stuff goes here -*-
 
+        # Integration tests for MemberFolder
+        ztc.ZopeDocFileSuite(
+            'MemberFolder.txt',
+            package='hph.membership',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+
         # Integration tests for Workspace
         ztc.ZopeDocFileSuite(
             'Workspace.txt',
