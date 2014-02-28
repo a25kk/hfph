d853975501bd200d52f8dc970cf8e969500fdf70
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-28 13:56:12 +0100

    Update workspace type to be usable

--- src/hph.membership/hph/membership/workspace.py
+++ src/hph.membership/hph/membership/workspace.py
@@ -21,48 +21,18 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from hph.membership import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class IWorkspace(form.Schema, IImageScaleTraversable):
     """
     Personal workspace container for members
     """
 
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/workspace.xml to define the content type.
-
-    form.model("models/workspace.xml")
-
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class Workspace(Container):
     grok.implements(IWorkspace)
-
-    # Add your class methods and properties here
     pass
 
 
-# View class
-# The view will automatically use a similarly named template in
-# workspace_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
 class View(grok.View):
-    """ sample view class """
-
     grok.context(IWorkspace)
-    grok.require('zope2.View')
+    grok.require('cmf.ModifyPortalContent')
     grok.name('view')
-
-    # Add view methods here
