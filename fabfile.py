2d80364815d76551fa3f6cd052b3170df0463a38
bumblebee831 <eg@kreativkombinat.de>
2014-03-02 19:20:47 +0100

    Update

--- fabfile.py
+++ fabfile.py
@@ -11,15 +11,16 @@ from ade25.fabfiles.server import setup
 
 env.use_ssh_config = True
 env.forward_agent = True
-# env.port = '22222'
+env.port = '22222'
 env.user = 'root'
 env.hosts = ['z9']
 env.webserver = '/opt/webserver/buildout.webserver'
 env.code_root = '/opt/sites/hph/buildout.hph'
-env.local_root = '/Users/cb/dev/hph/buildout.hph'
+env.local_root = '/Users/eva/dev/hfph/buildout.hfph'
 env.sitename = 'hph'
 env.code_user = 'root'
 env.prod_user = 'www'
+
 
 env.roledefs = {
     'production': ['hph'],
