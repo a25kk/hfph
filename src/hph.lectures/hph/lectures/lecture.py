a53675da923df580891890b5283476cea100b17d
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-03-25 15:30:53 +0100

    Cleanup lecture view

--- src/hph.lectures/hph/lectures/lecture.py
+++ src/hph.lectures/hph/lectures/lecture.py
@@ -2,19 +2,13 @@ from Acquisition import aq_inner
 from five import grok
 from plone import api
 
-from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import getVocabularyRegistry
 
 from plone.indexer import indexer
 from plone.dexterity.content import Container
 
-from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.directives import form
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from plone.namedfile.interfaces import IImageScaleTraversable
 
@@ -23,7 +17,6 @@ from z3c.relationfield.schema import RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 
 from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
-from collective.z3cform.widgets.token_input_widget import TokenInputFieldWidget
 
 from hph.faculty.facultymember import IFacultyMember
 
@@ -155,6 +148,17 @@ class View(grok.View):
         context = aq_inner(self.context)
         vr = getVocabularyRegistry()
         vocab = vr.get(context, 'hph.lectures.CourseDuration')
+        title = _(u"undefined")
+        if value is not None:
+            for term in vocab:
+                if term.value == value:
+                    title = term.title
+        return title
+
+    def prettify_degree(self, value):
+        context = aq_inner(self.context)
+        vr = getVocabularyRegistry()
+        vocab = vr.get(context, 'hph.lectures.CourseDegree')
         title = _(u"undefined")
         if value is not None:
             for term in vocab:
