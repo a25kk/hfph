1acff27a4322dadec473f1627a7eb806fde553cc
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-03-26 10:48:21 +0100

    Add small fix to trigger invitation mails

--- src/hph.membership/hph/membership/invitation.py
+++ src/hph.membership/hph/membership/invitation.py
@@ -238,7 +238,7 @@ class InviteNewUser(grok.View):
 
     def _compose_invitation_message(self, user_id):
         user = api.user.get(username=user_id)
-        token = self._access_token()
+        token = self._access_token(user)
         portal_url = api.portal.get().absolute_url()
         url = '{0}/useraccount/{1}/{2}'.format(
             portal_url, user_id, token)
