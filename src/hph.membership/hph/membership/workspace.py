6e2d16df77eee8281579da2473dfda46b385cfb7
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-04-08 17:12:15 +0200

    Update translations and refactor user workspaces

--- src/hph.membership/hph/membership/workspace.py
+++ src/hph.membership/hph/membership/workspace.py
@@ -5,6 +5,7 @@ from plone import api
 from plone.dexterity.content import Container
 
 from plone.directives import form
+from plone.keyring import django_random
 from plone.namedfile.interfaces import IImageScaleTraversable
 
 from hph.membership import MessageFactory as _
@@ -39,7 +40,7 @@ class View(grok.View):
         user = api.user.get(username=userid)
         info['fullname'] = user.getProperty('fullname', '') or userid
         info['email'] = user.getProperty('email', _(u"No email provided"))
-        info['login_time'] = user.getProperty('login_time', '')
+        info['login_time'] = user.getProperty('last_login_time', '')
         info['enabled'] = user.getProperty('enabled', '')
         info['confirmed'] = user.getProperty('confirmed', '')
         return info
@@ -57,3 +58,22 @@ class View(grok.View):
             if role in admin_roles:
                 is_adm = True
         return is_adm
+
+    def compose_pwreset_link(self):
+        context = aq_inner(self.context)
+        user_id = context.getId()
+        user = api.user.get(username=user_id)
+        token = self._access_token(user)
+        portal_url = api.portal.get().absolute_url()
+        url = '{0}/set-user-password/{1}/{2}'.format(
+            portal_url, user_id, token)
+        return url
+
+    def _access_token(self, user):
+        stored_token = user.getProperty('token', '')
+        if len(stored_token):
+            token = stored_token
+        else:
+            token = django_random.get_random_string(length=12)
+            user.setMemberProperties(mapping={'token': token})
+        return token
