3ce0178fb8344953f9532171af2b2d226d08b7a4
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-05-02 15:37:55 +0200

    Change event date query

--- src/hph.sitecontent/hph/sitecontent/frontpage.py
+++ src/hph.sitecontent/hph/sitecontent/frontpage.py
@@ -91,7 +91,7 @@ class RecentEventsView(grok.View):
                               query='/'.join(container.getPhysicalPath()),
                               depth=1),
                           review_state='published',
-                          start={'query': DateTime.DateTime(), 'range': 'max'},
+                          end={'query': DateTime.DateTime(), 'range': 'max'},
                           sort_on='start',
                           sort_limit=3)[:3]
         return results
