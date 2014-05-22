6ae84edb6c3e34055acd9cae8989f6ca104e154e
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-05-22 20:50:22 +0200

    Add token field to controlpanel
    We use a dedicated token that will be passed by the external request
    when issueing a api user record update

--- src/hph.membership/hph/membership/interfaces.py
+++ src/hph.membership/hph/membership/interfaces.py
@@ -20,3 +20,9 @@ class IHPHMembershipSettings(Interface):
     api_key = schema.TextLine(
         title=_(u"MembershipTool API Key"),
     )
+    api_token = schema.TextLine(
+        title=_(u"Membership Tool API Token"),
+        description=_(u"This API token can be used to authenticate with the "
+                      u"portal e.g. when calling the member updater via "
+                      u"external cronjob"),
+    )
