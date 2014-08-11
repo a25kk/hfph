ea292786482466a148fb42d0e80bc842e5270e30
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-08-11 23:55:34 +0200

    Start lecture factory integration
    We provide standalone forms for lecture contributors that will use
    plone.api.env.adopt_roles under the hood to allow for restricted
    edit forms for listed contributors

--- src/hph.lectures/hph/lectures/factory.py
+++ src/hph.lectures/hph/lectures/factory.py
@@ -1,16 +1,20 @@
 # -*- coding: UTF-8 -*-
+"""Module to allow contributions on lecture content"""
+
 from Acquisition import aq_inner
 from five import grok
-from hph.lectures.lecture import ILecture
-from hph.membership.workspace import IWorkspace
 from plone import api
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.directives import form
 from z3c.form import button
-from zope import schema
 from zope.component import getUtility
 from zope.lifecycleevent import modified
 from zope.schema import getFieldsInOrder
+
+from hph.lectures.lecture import ILecture
+from hph.lectures.coursedetails import ICourseDetails
+from hph.lectures.coursemodules import ICourseModuleInformation
+from hph.membership.workspace import IWorkspace
 
 from hph.lectures import MessageFactory as _
 
@@ -31,15 +35,15 @@ class LectureFactory(grok.View):
         return self
 
 
-class PanelHeadingEditForm(form.SchemaEditForm):
+class LectureBaseEditForm(form.SchemaEditForm):
     grok.context(IWorkspace)
     grok.require('cmf.AddPortalContent')
-    grok.name('panel-heading')
+    grok.name('lecture-base')
 
     schema = ILecture
-    ignoreContext = False
+    ignoreContext = True
     css_class = 'app-form'
-    label = _(u"Edit content panel")
+    label = _(u"Edit course base information")
 
     @property
     def traverse_subpath(self):
@@ -58,7 +62,7 @@ class PanelHeadingEditForm(form.SchemaEditForm):
             context.absolute_url(), row)
         return url
 
-    def panel(self):
+    def content_item(self):
         uid = self.traverse_subpath[2]
         item = api.content.get(UID=uid)
         return item
@@ -69,24 +73,23 @@ class PanelHeadingEditForm(form.SchemaEditForm):
         if errors:
             self.status = self.formErrorsMessage
             return
-        item = self.panel()
+        item = self.content_item()
         setattr(item, 'textline', data['textline'])
         modified(item)
         item.reindexObject(idxs='modified')
-        api.portal.show_message(_(u"The panel has successfully been updated"),
+        api.portal.show_message(_(u"The item has successfully been updated"),
                                 self.request,
                                 type='info')
         return self.request.response.redirect(self.next_url())
 
     @button.buttonAndHandler(_(u"cancel"))
     def handleCancel(self, action):
-        msg = _(u"Content panel factory has been cancelled.")
+        msg = _(u"Content item factory has been cancelled.")
         api.portal.show_message(msg, self.request, type='info')
         return self.request.response.redirect(self.next_url())
 
     def getContent(self):
-        uid = self.traverse_subpath[2]
-        item = api.content.get(UID=uid)
+        item = self.content_item()
         fti = getUtility(IDexterityFTI,
                          name='ade25.panelpage.panel')
         schema = fti.lookupSchema()
@@ -94,5 +97,4 @@ class PanelHeadingEditForm(form.SchemaEditForm):
         data = {}
         for key, value in fields:
             data[key] = getattr(item, key, value)
-        data['textline'] = getattr(item, 'textline', '')
         return data
