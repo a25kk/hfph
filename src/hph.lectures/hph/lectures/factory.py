684a8a96a1f33ec74a93e3dc4d5d77f1c1fe4786
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-09-03 14:21:33 +0200

    Prevent edit errors using custom schema

--- src/hph.lectures/hph/lectures/factory.py
+++ src/hph.lectures/hph/lectures/factory.py
@@ -7,6 +7,8 @@ from plone import api
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.directives import form
 from z3c.form import button
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from zope import schema
 from zope.component import getUtility
 from zope.lifecycleevent import modified
 from zope.schema import getFieldsInOrder
@@ -40,12 +42,92 @@ class LectureFactory(grok.View):
         return item
 
 
+class ILectureBase(form.Schema):
+    """
+    A single course
+    """
+    subtitle = schema.TextLine(
+        title=_(u"Subtitle"),
+        required=False,
+    )
+    title = schema.TextLine(
+        title=_(u"Title"),
+        required=True,
+    )
+    courseNumber = schema.TextLine(
+        title=_(u"Lecure Number"),
+        required=True,
+    )
+    attendance = schema.TextLine(
+        title=_(u"Attendance"),
+        required=False,
+    )
+    attendanceRequired = schema.Bool(
+        title=_(u"Attendance Required"),
+        required=False,
+    )
+    blockLecture = schema.Bool(
+        title=_(u"Block Lecture"),
+        required=False,
+    )
+    description = schema.Text(
+        title=_(u"Additional Information"),
+        required=False,
+    )
+    lecturerAdditional = schema.TextLine(
+        title=_(u"Additional Lecturer"),
+        required=False,
+    )
+    courseType = schema.Choice(
+        title=_(u"Course Type"),
+        description=_(u"Please select the course type."),
+        required=False,
+        vocabulary=u'hph.lectures.CourseType',
+    )
+    courseDuration = schema.Choice(
+        title=_(u"Course Duration"),
+        required=False,
+        vocabulary=u'hph.lectures.CourseDuration',
+    )
+    courseDates = schema.TextLine(
+        title=_(u"Course Dates"),
+        required=True,
+    )
+    courseTime = schema.TextLine(
+        title=_(u"Course Time"),
+        required=True,
+    )
+    courseSemester = schema.Choice(
+        title=_(u"Course Semester"),
+        description=_(u"Please select the course semester for filtering"),
+        required=True,
+        vocabulary=u'hph.lectures.CourseSemester',
+    )
+    courseYear = schema.TextLine(
+        title=_(u"Course Year"),
+        required=True,
+    )
+    courseRoom = schema.TextLine(
+        title=_(u"Course Room"),
+        required=False,
+    )
+    form.widget(thirdPartyProject=CheckBoxFieldWidget)
+    thirdPartyProject = schema.Set(
+        title=_(u"Third Party Project Display"),
+        value_type=schema.Choice(
+            title=_(u"Display Selection"),
+            vocabulary=u'hph.sitecontent.thirdPartyProjects',
+        ),
+        required=False,
+    )
+
+
 class LectureBaseEditForm(form.SchemaEditForm):
     grok.context(IWorkspace)
     grok.require('cmf.AddPortalContent')
     grok.name('lecture-base')
 
-    schema = ILecture
+    schema = ILectureBase
     ignoreContext = True
     css_class = 'app-form'
     label = _(u"Edit course base information")
@@ -96,7 +178,7 @@ class LectureBaseEditForm(form.SchemaEditForm):
     def getContent(self):
         item = self.content_item()
         fti = getUtility(IDexterityFTI,
-                         name='ade25.panelpage.panel')
+                         name='hph.lectures.lecture')
         schema = fti.lookupSchema()
         fields = getFieldsInOrder(schema)
         data = {}
