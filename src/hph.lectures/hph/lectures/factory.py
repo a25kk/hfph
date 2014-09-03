bbd86628e78341ff3447c6e9063a9ea452638a14
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-09-03 14:32:57 +0200

    Move schema registration to dedicated module

--- src/hph.lectures/hph/lectures/factory.py
+++ src/hph.lectures/hph/lectures/factory.py
@@ -7,15 +7,11 @@ from plone import api
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.directives import form
 from z3c.form import button
-from z3c.form.browser.checkbox import CheckBoxFieldWidget
-from zope import schema
 from zope.component import getUtility
 from zope.lifecycleevent import modified
 from zope.schema import getFieldsInOrder
 
-from hph.lectures.lecture import ILecture
-from hph.lectures.coursedetails import ICourseDetails
-from hph.lectures.coursemodules import ICourseModuleInformation
+from hph.lectures.interfaces import ILectureBase
 from hph.membership.workspace import IWorkspace
 
 from hph.lectures import MessageFactory as _
@@ -40,86 +36,6 @@ class LectureFactory(grok.View):
         uid = self.traverse_subpath[0]
         item = api.content.get(UID=uid)
         return item
-
-
-class ILectureBase(form.Schema):
-    """
-    A single course
-    """
-    subtitle = schema.TextLine(
-        title=_(u"Subtitle"),
-        required=False,
-    )
-    title = schema.TextLine(
-        title=_(u"Title"),
-        required=True,
-    )
-    courseNumber = schema.TextLine(
-        title=_(u"Lecure Number"),
-        required=True,
-    )
-    attendance = schema.TextLine(
-        title=_(u"Attendance"),
-        required=False,
-    )
-    attendanceRequired = schema.Bool(
-        title=_(u"Attendance Required"),
-        required=False,
-    )
-    blockLecture = schema.Bool(
-        title=_(u"Block Lecture"),
-        required=False,
-    )
-    description = schema.Text(
-        title=_(u"Additional Information"),
-        required=False,
-    )
-    lecturerAdditional = schema.TextLine(
-        title=_(u"Additional Lecturer"),
-        required=False,
-    )
-    courseType = schema.Choice(
-        title=_(u"Course Type"),
-        description=_(u"Please select the course type."),
-        required=False,
-        vocabulary=u'hph.lectures.CourseType',
-    )
-    courseDuration = schema.Choice(
-        title=_(u"Course Duration"),
-        required=False,
-        vocabulary=u'hph.lectures.CourseDuration',
-    )
-    courseDates = schema.TextLine(
-        title=_(u"Course Dates"),
-        required=True,
-    )
-    courseTime = schema.TextLine(
-        title=_(u"Course Time"),
-        required=True,
-    )
-    courseSemester = schema.Choice(
-        title=_(u"Course Semester"),
-        description=_(u"Please select the course semester for filtering"),
-        required=True,
-        vocabulary=u'hph.lectures.CourseSemester',
-    )
-    courseYear = schema.TextLine(
-        title=_(u"Course Year"),
-        required=True,
-    )
-    courseRoom = schema.TextLine(
-        title=_(u"Course Room"),
-        required=False,
-    )
-    form.widget(thirdPartyProject=CheckBoxFieldWidget)
-    thirdPartyProject = schema.Set(
-        title=_(u"Third Party Project Display"),
-        value_type=schema.Choice(
-            title=_(u"Display Selection"),
-            vocabulary=u'hph.sitecontent.thirdPartyProjects',
-        ),
-        required=False,
-    )
 
 
 class LectureBaseEditForm(form.SchemaEditForm):
---
+++ src/hph.lectures/hph/lectures/interfaces.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+"""Lecture edit form schema interfaces"""
+
+from plone.directives import form
+from zope import schema
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+
+from hph.lectures import MessageFactory as _
+
+
+class ILectureBase(form.Schema):
+    """
+    A single course
+    """
+    subtitle = schema.TextLine(
+        title=_(u"Subtitle"),
+        required=False,
+    )
+    title = schema.TextLine(
+        title=_(u"Title"),
+        required=True,
+    )
+    courseNumber = schema.TextLine(
+        title=_(u"Lecure Number"),
+        required=True,
+    )
+    attendance = schema.TextLine(
+        title=_(u"Attendance"),
+        required=False,
+    )
+    attendanceRequired = schema.Bool(
+        title=_(u"Attendance Required"),
+        required=False,
+    )
+    blockLecture = schema.Bool(
+        title=_(u"Block Lecture"),
+        required=False,
+    )
+    description = schema.Text(
+        title=_(u"Additional Information"),
+        required=False,
+    )
+    lecturerAdditional = schema.TextLine(
+        title=_(u"Additional Lecturer"),
+        required=False,
+    )
+    courseType = schema.Choice(
+        title=_(u"Course Type"),
+        description=_(u"Please select the course type."),
+        required=False,
+        vocabulary=u'hph.lectures.CourseType',
+    )
+    courseDuration = schema.Choice(
+        title=_(u"Course Duration"),
+        required=False,
+        vocabulary=u'hph.lectures.CourseDuration',
+    )
+    courseDates = schema.TextLine(
+        title=_(u"Course Dates"),
+        required=True,
+    )
+    courseTime = schema.TextLine(
+        title=_(u"Course Time"),
+        required=True,
+    )
+    courseSemester = schema.Choice(
+        title=_(u"Course Semester"),
+        description=_(u"Please select the course semester for filtering"),
+        required=True,
+        vocabulary=u'hph.lectures.CourseSemester',
+    )
+    courseYear = schema.TextLine(
+        title=_(u"Course Year"),
+        required=True,
+    )
+    courseRoom = schema.TextLine(
+        title=_(u"Course Room"),
+        required=False,
+    )
+    form.widget(thirdPartyProject=CheckBoxFieldWidget)
+    thirdPartyProject = schema.Set(
+        title=_(u"Third Party Project Display"),
+        value_type=schema.Choice(
+            title=_(u"Display Selection"),
+            vocabulary=u'hph.sitecontent.thirdPartyProjects',
+        ),
+        required=False,
+    )
