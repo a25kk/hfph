11bcadc98f7b70120e5e68e5ed320b2e9f03af05
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-10-13 17:21:12 +0200

    Add ajax submit link function for medium editor
    We need to submit the contents of the medium editor div to a view
    that stores the data.

--- src/hph.corporatetheme/hph/corporatetheme/resources/js/application.js
+++ src/hph.corporatetheme/hph/corporatetheme/resources/js/application.js
@@ -1,40 +1,32 @@
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false */
 'use strict';
-
 (function ($) {
     $(document).ready(function () {
-        if ($('body').hasClass('lt-ie7')) {return; }
-        // Application specific javascript code goes here
+        if ($('body').hasClass('lt-ie7')) {
+            return;
+        }
         if ($('.bs-docs-top').length > 0) {
             setTimeout(function () {
                 $('.bs-docs-top').affix();
             }, 100);
         }
         $('#app-toolbar').headroom();
-        $('.marquee').marquee({
-            speed: 5000
-        });
+        $('.marquee').marquee({ speed: 5000 });
         var $ajaxContainer = $('#appui-container');
         $('a[data-appui="pjaxed"]').each(function () {
-            var $targetUrl = $(this).attr('href'),
-                $hideEl = $(this).data('appui-hide');
+            var $targetUrl = $(this).attr('href'), $hideEl = $(this).data('appui-hide');
             $(this).on('click', function (e) {
                 e.preventDefault();
                 $(this).addClass('selected');
                 $('#app-box-footer').removeClass('hide').addClass('show');
                 $($hideEl).hide();
-                $ajaxContainer.load($targetUrl + '?ajax_load=1 #content-core >*'
-                    ).fadeIn('slow');
+                $ajaxContainer.load($targetUrl + '?ajax_load=1 #content-core >*').fadeIn('slow');
             });
         });
         $('div[data-appui="eventbox"]').each(function () {
-            var $sourceUrl = $(this).data('source'),
-                $targetEl = $(this);
+            var $sourceUrl = $(this).data('source'), $targetEl = $(this);
             $.getJSON($sourceUrl, function (data) {
                 var divData = '';
                 $.each(data.items, function (i, item) {
-                    //alert('Item:' + item.title);
                     divData += '<a class="app-box-item" href="' + item.url + '">';
                     divData += '<time class="app-box-date h5">' + item.date + '</time>';
                     divData += '<span>' + item.title + '</span>';
@@ -44,11 +36,25 @@
                 $targetEl.html(divData);
             });
         });
+        $('a[data-appui="ajax-submit"]').on('click', function (e) {
+            e.preventDefault();
+            var $targetURL = $(this).data('appui-target-url');
+            $.ajax({
+                url: $targetURL,
+                data: $(this).serializeArray(),
+                success: function (res) {
+                    if (res.result === 'ok') {
+                        $console.text('Data saved');
+                    } else {
+                        $console.text('Save error');
+                    }
+                }
+            });
+        });
         $('a[data-appui="overslide"]').on({
             click: function (e) {
                 e.preventDefault();
                 var targetBlock = $(this).data('target');
-                // $(this).parent().removeClass('bounceInLeft').addClass('bounceOutRight');
                 if ($(targetBlock).hasClass('fadeOutTop')) {
                     $(targetBlock).removeClass('fadeOutTop').addClass('slideInRight').show();
                 } else {
@@ -62,6 +68,5 @@
                 $(this).closest('.panelpage-slide').removeClass('slideInRight').addClass('fadeOutTop').hide();
             }
         });
-    }
-    );
+    });
 }(jQuery));
