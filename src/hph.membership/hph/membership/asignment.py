0c3ae3d5bf2873419c8be58b9306ff4a2a2820c9
Christoph Boehner <cb@vorwaerts-werbung.de>
2015-02-19 14:49:54 +0100

    Refactor and refine asignment handler view
    refs #3

--- src/hph.membership/hph/membership/asignment.py
+++ src/hph.membership/hph/membership/asignment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Module providing responsible user selection and asignment"""
 
+from Acquisition import aq_inner
 from five import grok
 from plone import api
 from Products.CMFCore.interfaces import IContentish
@@ -48,6 +49,7 @@ class AsignmentUsers(grok.View):
         return api.user.get_users(groupname=self.groupname())
 
     def selectable_users(self):
+        context = aq_inner(self.context)
         users = []
         idx = 0
         for record in self.group_users():
@@ -59,8 +61,16 @@ class AsignmentUsers(grok.View):
             info['userid'] = userid
             info['fullname'] = user.getProperty('fullname', '') or userid
             info['email'] = user.getProperty('email', _(u"No email provided"))
-            user.append(info)
+            info['roles'] = api.user.get_roles(username=userid, obj=context)
+            users.append(info)
         return users
+
+    def get_asignment(self, userid):
+        context = aq_inner(self.context)
+        roles = api.user.get_roles(username=userid, obj=context)
+        if 'Contributor' in roles:
+            return True
+        return False
 
 
 class Asignment(grok.View):
@@ -68,3 +78,37 @@ class Asignment(grok.View):
     grok.context(IContentish)
     grok.require('cmf.ManagePortal')
     grok.name('asignment')
+
+    @property
+    def traverse_subpath(self):
+        return self.subpath
+
+    def publishTraverse(self, request, name):
+        if not hasattr(self, 'subpath'):
+            self.subpath = []
+        self.subpath.append(name)
+        return self
+
+    def render(self):
+        context = aq_inner(self.context)
+        user_roles = ['Reader', 'Editor', 'Contributor']
+        userid = self.traverse_subpath[0]
+        action = self.traverse_subpath[1]
+        if action == 'revoke':
+            api.user.revoke_roles(
+                username=userid,
+                roles=user_roles,
+                obj=context,
+            )
+        else:
+            api.user.grant_roles(
+                username=userid,
+                roles=user_roles,
+                obj=context
+            )
+        next_url = '{0}/@asignment-view?updated=true'.format(
+            context.absolute_url())
+        api.portal.show_message(
+            message=_(u"Responsible user asignment successfully updated"),
+            request=self.request)
+        return self.request.response.redirect(next_url)
