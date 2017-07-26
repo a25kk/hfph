57193092104fed4f161728bfabbcb7afc22b03d8
Christoph Böhner-Figas <boehner@team23.de>
2017-07-26 17:35:36 +0200

    Update course view

--- src/hph.lectures/hph/lectures/browser/course.py
+++ src/hph.lectures/hph/lectures/browser/course.py
@@ -128,7 +128,11 @@ class CourseView(BrowserView):
             modules = vocabulary.learning_modules_bachelor()
         else:
             modules = vocabulary.learning_modules_master()
-        return modules[module]
+        try:
+            module_title = modules[module]
+        except KeyError:
+            module_title = module
+        return module_title
 
     def has_module_data(self):
         return len(self.module_data()) > 0
