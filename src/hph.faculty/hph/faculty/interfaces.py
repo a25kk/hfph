cc2a49d1f0ba76873bf69abbb89a49038c3a74bf
Christoph Böhner-Figas <boehner@team23.de>
2017-08-02 15:13:21 +0200

    Refactor faculty package

---
+++ src/hph.faculty/hph/faculty/interfaces.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+"""Lecture edit form schema interfaces"""
+
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IHPHFacultyLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer."""
