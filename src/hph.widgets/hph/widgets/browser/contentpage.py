8ff8561eaa34d190128d2501629a4e664e515b05
Christoph Böhner-Figas <boehner@team23.de>
2019-02-19 14:49:28 +0100

    Add widget package

---
+++ src/hph.widgets/hph/widgets/browser/contentpage.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Module providing views for the folderish content page type"""
+from Products.Five.browser import BrowserView
+
+
+class ContentPageView(BrowserView):
+    """ Folderish content page default view """
