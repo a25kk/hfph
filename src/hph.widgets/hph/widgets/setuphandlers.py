8e5823cc0a1a700e9313ac45877a5afa576f00eb
Kreativkombinat GbR <info@kreativkombinat.de>
2019-02-22 12:18:54 +0100

    Deploying '7c86fcd8d6cd2dd442e2574ae1a737dcd252b07d' from branch 'staging' into 'release/staging'. Date: 2019-02-22T12:18:53.662207

---
+++ src/hph.widgets/hph/widgets/setuphandlers.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Module providing custom setup steps"""
+import json
+
+from plone import api
+
+
+def register_content_widgets(site):
+    """Run custom add-on package installation code to add custom
+       site specific content widgets
+
+    @param site: Plone site
+    """
+    content_widgets = ['teaser-news', ]
+    widget_settings = api.portal.get_registry_record(
+        name='ade25.widgets.widget_settings'
+    )
+    stored_widgets = json.loads(widget_settings)
+    for widget_type in content_widgets:
+        if widget_type not in stored_widgets:
+            # Generate default settings via widgets tool
+            stored_widgets.append(widget_type)
+    api.portal.set_registry_record(
+        name='ade25.base.listed_content_types',
+        value=json.dumps(stored_widgets)
+    )
+
+
+def setup_various(context):
+    """
+    @param context: Products.GenericSetup.context.DirectoryImportContext instance
+    """
+
+    # We check from our GenericSetup context whether we are running
+    # add-on installation for your package or any other
+    if context.readDataFile('hph.widgets.marker.txt') is None:
+        # Not your add-on
+        return
+
+    portal = context.getSite()
+
+    register_content_widgets(portal)
