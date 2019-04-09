5b3b7fa2caebffb09a3f27a8796c4daf7fc151b9
Kreativkombinat GbR <info@kreativkombinat.de>
2019-04-09 10:54:41 +0200

    Deploying 'e653b88a773a915c13a353e3680b59b8f54924c3' from branch 'staging' into 'release/staging'. Date: 2019-04-09T10:54:40.910855

--- src/hph.widgets/hph/widgets/widgets/teaser/teaser.py
+++ src/hph.widgets/hph/widgets/widgets/teaser/teaser.py
@@ -114,3 +114,111 @@ class WidgetTeaserNews(BrowserView):
                 'item_object': brain.getObject()
             })
         return results
+
+
+class WidgetTeaserLinksInternal(BrowserView):
+    """ Base widget used as placeholder """
+
+    def __call__(self,
+                 widget_name='teaser-links-internal',
+                 widget_type='teaser-links-internal',
+                 widget_mode='view',
+                 widget_data=None,
+                 **kw):
+        self.params = {
+            'widget_name': widget_name,
+            'widget_type': widget_type,
+            'widget_mode': widget_mode,
+            'widget_data': widget_data
+        }
+        return self.render()
+
+    def render(self):
+        return self.index()
+
+    @property
+    def edit_mode(self):
+        if self.params['widget_mode'] == 'edit':
+            return True
+        return False
+
+    @property
+    def record(self):
+        return self.params['widget_data']
+
+    def has_content(self):
+        if self.widget_content_items():
+            return True
+        return False
+
+    def widget_uid(self):
+        try:
+            widget_id = self.record['id']
+        except (KeyError, TypeError):
+            widget_id = str(uuid_tool.uuid4())
+        return widget_id
+
+    def widget_content_items(self):
+        return self.recent_news()
+
+    def widget_custom_styles(self):
+        if self.record and 'styles' in self.record:
+            return self.record['styles']
+        else:
+            return None
+
+    def widget_content_list_class(self):
+        context = aq_inner(self.context)
+        css_class = 'c-list c-list--gutter c-list--grid c-list--{}'.format(
+            context.UID())
+        custom_styles = self.widget_custom_styles()
+        if custom_styles:
+            class_container = custom_styles['class_container']
+            for class_name in class_container.split(' '):
+                css_class = '{0} c-list--{1}'.format(
+                    css_class,
+                    class_name
+                )
+            if 'custom' in custom_styles:
+                css_class = '{0} {1}'.format(
+                    css_class,
+                    custom_styles['custom']
+                )
+        return css_class
+
+    @staticmethod
+    def time_stamp(item, date_time):
+        content_info_provider = IContentInfoProvider(item)
+        time_stamp = content_info_provider.time_stamp(date_time)
+        return time_stamp
+
+    @staticmethod
+    def get_latest_news_items(limit=3):
+        portal = api.portal.get()
+        items = api.content.find(
+            context=portal,
+            object_provides=INewsEntry.__identifier__,
+            review_state='published',
+            sort_on='Date',
+            sort_order='reverse',
+            sort_limit=limit
+        )[:limit]
+        return items
+
+    def recent_news(self):
+        results = []
+        brains = self.get_latest_news_items()
+        for brain in brains:
+            results.append({
+                'title': brain.Title,
+                'description': brain.Description,
+                'url': brain.getURL(),
+                'timestamp': brain.Date,
+                'uuid': brain.UID,
+                "css_classes": "o-card-list__item--{0}".format(
+                    brain.UID
+                ),
+                'item_object': brain.getObject()
+            })
+        return results
+
