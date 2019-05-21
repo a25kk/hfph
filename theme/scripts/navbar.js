30d7dbab48abdea332e4c15704b3dfeb7161446a
Christoph Böhner-Figas <boehner@team23.de>
2019-05-21 21:57:56 +0200

    Update navbar click handler
    Remove defunct touchstart solution in order to restore the
    original version

--- theme/scripts/navbar.js
+++ theme/scripts/navbar.js
@@ -194,9 +194,9 @@ define([
         document.addEventListener('click', function (event) {
             handleBackDropEvent(options, event, bodyElement);
         });
-        document.addEventListener('touchstart', function (event) {
-            handleBackDropEvent(options, event, bodyElement);
-        });
+        // document.addEventListener('touchstart', function (event) {
+        //     handleBackDropEvent(options, event, bodyElement);
+        // });
         // Nav bar toggle
         navBarToggle.forEach(function(el) {
             el.addEventListener("click", function(event) {
