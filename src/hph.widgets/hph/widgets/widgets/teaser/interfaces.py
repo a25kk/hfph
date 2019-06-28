d93c0b994542097ecdcacd2dee3a596fe68f9869
Kreativkombinat GbR <info@kreativkombinat.de>
2019-06-28 09:34:25 +0200

    Deploying 'f79b695b624f1fd53a9455ad646f7eb312bbdef5' from branch 'staging' into 'release/staging'. Date: 2019-06-28T09:34:24.752801

--- src/hph.widgets/hph/widgets/widgets/teaser/interfaces.py
+++ src/hph.widgets/hph/widgets/widgets/teaser/interfaces.py
@@ -5,6 +5,7 @@ from plone.autoform import directives as form, directives
 from plone.app.z3cform.widget import LinkFieldWidget
 from zope import schema
 from zope.interface import Interface, provider
+from plone.namedfile import field as named_file
 
 from ade25.panelpage import MessageFactory as _
 
@@ -34,6 +35,30 @@ class IHPHWidgetLinkInternal(Interface):
         required=False,
         default='widget--tile',
         vocabulary='hph.widgets.vocabularies.TeaserLinkIconOptions'
+    )
+    directives.widget(link=LinkFieldWidget)
+    link = schema.TextLine(
+        title=_(u"Link"),
+        description=_(u"Please select link target"),
+        required=False,
+    )
+
+
+@provider(IFormFieldProvider)
+class IHPHWidgetLinkExternal(Interface):
+    """ Content Panel Storage Slots """
+
+    title = schema.TextLine(
+        title=_("External Link Title"),
+        required=False
+    )
+    image = named_file.NamedBlobImage(
+        title=_(u"Image"),
+        required=True
+    )
+    image_caption = schema.TextLine(
+        title=_(u"Image Copyright Information"),
+        required=False
     )
     directives.widget(link=LinkFieldWidget)
     link = schema.TextLine(
