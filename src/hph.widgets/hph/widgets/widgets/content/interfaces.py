296cca1b8f8e07c47a960a2135c4702ed086cdbc
Kreativkombinat GbR <info@kreativkombinat.de>
2019-07-22 14:43:54 +0200

    Deploying '4a076fd0aa234121aa683818657ced36e5a1e71e' from branch 'staging' into 'release/staging'. Date: 2019-07-22T14:43:53.761025

---
+++ src/hph.widgets/hph/widgets/widgets/content/interfaces.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Module providing standalone content panel edit forms"""
+from plone.autoform.interfaces import IFormFieldProvider
+from zope import schema
+from zope.interface import Interface, provider
+
+from ade25.panelpage import MessageFactory as _
+
+
+@provider(IFormFieldProvider)
+class IHPHWidgetContentAlias(Interface):
+    """ Content Widget to display external content via references """
+
+    alias = schema.TextLine(
+        title=u"Content Source",
+        description=_(u"Please enter the unique identifier of the target "
+                      u"content item available via attaching @@uuid to the "
+                      u"target object's url"),
+        required=True,
+    )
