66de3e6ae5bae511a6facef4e3bd0e00f74333ce
Kreativkombinat GbR <info@kreativkombinat.de>
2019-08-05 22:13:08 +0200

    Deploying 'cfd2d70f436be4dc3190e431f7f037b317ac5ca8' from branch 'staging' into 'release/staging'. Date: 2019-08-05T22:13:07.917176

--- theme/scripts/navbar.js
+++ theme/scripts/navbar.js
@@ -199,13 +199,13 @@ define([
                 }
             }
         });
+        document.addEventListener('touchstart', function (event) {
+            handleBackDropEvent(options, event, bodyElement);
+        });
         // Close navigation via backdrop clicks
         document.addEventListener('click', function (event) {
             handleBackDropEvent(options, event, bodyElement);
         });
-        // document.addEventListener('touchstart', function (event) {
-        //     handleBackDropEvent(options, event, bodyElement);
-        // });
         // Nav bar toggle
         navBarToggle.forEach(function(el) {
             el.addEventListener("click", function(event) {
