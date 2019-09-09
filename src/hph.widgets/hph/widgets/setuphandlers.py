787d4114198563863e426eafc709ef1f5826ef10
Christoph Böhner-Figas <boehner@team23.de>
2019-09-09 14:50:39 +0200

    Update widget setup handler

--- src/hph.widgets/hph/widgets/setuphandlers.py
+++ src/hph.widgets/hph/widgets/setuphandlers.py
@@ -2,12 +2,16 @@
 """Module providing custom setup steps"""
 import datetime
 import json
+import logging
 import time
 
 import six
 
 from hph.widgets.config import PKG_WIDGETS
 from plone import api
+from plone.api.exc import InvalidParameterError
+
+logger = logging.getLogger(__name__)
 
 
 def register_content_widgets(site):
@@ -17,21 +21,28 @@ def register_content_widgets(site):
     @param site: Plone site
     """
     content_widgets = PKG_WIDGETS
-    widget_settings = api.portal.get_registry_record(
-        name='ade25.widgets.widget_settings'
-    )
-    stored_widgets = json.loads(widget_settings)
-    records = stored_widgets['items']
-    for content_widget, widget_data in content_widgets.items():
-        if content_widget not in records.keys():
-            records[content_widget] = widget_data
-    stored_widgets["items"] = records
-    stored_widgets["timestamp"] = six.text_type(int(time.time())),
-    stored_widgets["updated"] = datetime.datetime.now().isoformat(),
-    api.portal.set_registry_record(
-        name='ade25.widgets.widget_settings',
-        value=json.dumps(stored_widgets)
-    )
+    try:
+        widget_settings = api.portal.get_registry_record(
+            name=u"ade25.widgets.widget_settings"
+        )
+        stored_widgets = json.loads(widget_settings)
+        records = stored_widgets['items']
+        for content_widget, widget_data in content_widgets.items():
+            if content_widget not in records.keys():
+                records[content_widget] = widget_data
+        stored_widgets["items"] = records
+        stored_widgets["timestamp"] = six.text_type(int(time.time())),
+        stored_widgets["updated"] = datetime.datetime.now().isoformat(),
+        api.portal.set_registry_record(
+            name='ade25.widgets.widget_settings',
+            value=json.dumps(stored_widgets)
+        )
+    except InvalidParameterError:
+        logger.log(
+            "warning",
+            "Updated widget registration not possible due to missing "
+            "registry entry."
+        )
 
 
 def setup_various(context):
