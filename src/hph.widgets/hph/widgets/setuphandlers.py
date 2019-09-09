f1178042538d23b80d9bb2c16133ced18ab907e1
Christoph Böhner-Figas <boehner@team23.de>
2019-09-09 15:11:38 +0200

    Update widget setup handler

--- src/hph.widgets/hph/widgets/setuphandlers.py
+++ src/hph.widgets/hph/widgets/setuphandlers.py
@@ -6,6 +6,7 @@ import logging
 import time
 
 import six
+from Products.CMFPlone.utils import safe_unicode
 
 from hph.widgets.config import PKG_WIDGETS
 from plone import api
@@ -21,28 +22,21 @@ def register_content_widgets(site):
     @param site: Plone site
     """
     content_widgets = PKG_WIDGETS
-    try:
-        widget_settings = api.portal.get_registry_record(
-            name=u"ade25.widgets.widget_settings"
-        )
-        stored_widgets = json.loads(widget_settings)
-        records = stored_widgets['items']
-        for content_widget, widget_data in content_widgets.items():
-            if content_widget not in records.keys():
-                records[content_widget] = widget_data
-        stored_widgets["items"] = records
-        stored_widgets["timestamp"] = six.text_type(int(time.time())),
-        stored_widgets["updated"] = datetime.datetime.now().isoformat(),
-        api.portal.set_registry_record(
-            name='ade25.widgets.widget_settings',
-            value=json.dumps(stored_widgets)
-        )
-    except InvalidParameterError:
-        logger.log(
-            "warning",
-            "Updated widget registration not possible due to missing "
-            "registry entry."
-        )
+    widget_settings = api.portal.get_registry_record(
+        name="ade25.widgets.widget_settings"
+    )
+    stored_widgets = json.loads(widget_settings)
+    records = stored_widgets['items']
+    for content_widget, widget_data in content_widgets.items():
+        if content_widget not in records.keys():
+            records[content_widget] = widget_data
+    stored_widgets["items"] = records
+    stored_widgets["timestamp"] = six.text_type(int(time.time())),
+    stored_widgets["updated"] = datetime.datetime.now().isoformat(),
+    api.portal.set_registry_record(
+        name='ade25.widgets.widget_settings',
+        value=safe_unicode(json.dumps(stored_widgets))
+    )
 
 
 def setup_various(context):
