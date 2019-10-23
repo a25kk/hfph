c7ae389e9ec0ac51d068f83e98f0cbd96fc2e253
Kreativkombinat GbR <info@kreativkombinat.de>
2019-10-23 22:51:11 +0200

    Deploying '07dedbf1aee14c22e8dc710fe05ffd40c9322230' from branch 'production' into 'release/production'. Date: 2019-10-23T22:51:10.925472

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-virtualenv --clear .
+virtualenv -p python2.7 --clear .
 env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" ./bin/pip install cryptography
 ./bin/pip install -r requirements.txt
 ./bin/buildout -Nc development.cfg
