8dff626d7d43e29f81e7e47de3b4f110217a46c7
Christoph Böhner-Figas <boehner@team23.de>
2022-09-13 22:39:40 +0200

    feat(): Update to python3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-virtualenv -p python2.7 --clear .
-env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" ./bin/pip install cryptography
+`which python3` -m venv .
 ./bin/pip install -r requirements.txt
-./bin/buildout -Nc development.cfg
+./bin/buildout $*
+echo "run plone with: b5 plone"
